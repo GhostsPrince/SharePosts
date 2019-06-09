f80d5eb33b0778c58824b5fe8ad31c4376f55d41
Art <a_zakhariani@cu.edu.ge>
2019-06-09 23:41:25 +0400

    Add style

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,5 +1,23 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-  <h1><?php echo $data['title']; ?></h1>
-  <p><?php echo $data['description']; ?></p>
-  <p>Version : <strong><?php echo APPVERSION; ?><strong></p>
+<div class ="jumbotron jumbotron-fluid"
+     >
+    <div class="container text-center">
+        <h1 class="display-4">Some Info About This App</h1>
+        <hr class="my-4">
+    <h1><?php echo $data['title']; ?></h1>
+    <p class="lead"><?php echo $data['description']; ?></p>
+    <p class="lead"> Version : <strong><?php echo APPVERSION; ?><strong></p>
+        <hr>
+
+
+    </div>
+    <div class="container text-center">
+        <h6 class="display-4">Worked On It</h6>
+        <ul style="list-style: none;">
+            <li>Artur Zakharyan</li>
+            <li>Tristan Kadagishvili</li>
+            <li>Sandro Palavandishvili</li>
+        </ul>
+    </div>
+</div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,8 +1,19 @@
+<?php
+$currentTime = time();
+?>
 <?php require APPROOT . '/views/inc/header.php'; ?>
+
   <div class="jumbotron jumbotron-fluid text-center">
-    <div class="container">   
-      <h1 class="display-3"><?php echo $data['title']; ?></h1>
-      <p class="lead"><?php echo $data['description']; ?></p>
+    <div class="container mb-5  "
+        style="margin-top: -5%"
+    >
+        <div class="time float-right ">
+            <h6><?php echo date("l g:i a", $currentTime); ?></h6>
+            <h6><?php echo date("jS F, Y",$currentTime); ?></h6>
+        </div>
     </div>
-  </div>
+      <div class="container mt-5 ">
+          <h4 class="display-3"><?php echo $data['title']; ?></h4>
+      </div>
+</div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
